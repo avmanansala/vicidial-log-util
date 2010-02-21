af6f32efbc93ffe5367e4e4bd5bb15d297e82b1d
Lito Manansala <avmanansala@gmail.com>
2010-02-21 10:41:41 +0800

    first working and tested version

--- AST_CRON_mysql_log_cleanup.rb
+++ AST_CRON_mysql_log_cleanup.rb
@@ -8,9 +8,8 @@
 
 require ('rubygems')
 require ('parseconfig')
-require ('mysql')
 require ('active_record')
-
+require ('active_support')
 
 #Parse astguiclient configuration file.
 my_config = ParseConfig.new('/etc/astguiclient.conf')
@@ -21,22 +20,52 @@ mysql_user = my_config.get_value('VARDB_user').gsub('>','').strip
 mysql_pass = my_config.get_value('VARDB_pass').gsub('>','').strip 
 mysql_port = my_config.get_value('VARDB_port').gsub('>','').strip
 
-puts "Mysql server ip is #{mysql_server_ip}"
+puts "Mysql server ip is #{mysql_server_ip}\n"
 
  
 ActiveRecord::Base.establish_connection(
 :adapter => 'mysql',
 :host => mysql_server_ip,
 :database => mysql_database,
-:user => mysql_user,
+:username => mysql_user,
 :password => mysql_pass
 )
 
 #
-class Phones < ActiveRecord::Base
+class Vicidial_agent_log < ActiveRecord::Base 
+  set_table_name "vicidial_agent_log"
+end
+class Vicidial_user_log <  ActiveRecord::Base 
+  set_table_name "vicidial_user_log"
+end
+class Vicidial_admin_log <  ActiveRecord::Base 
+  set_table_name "vicidial_admin_log"
+end
+class Vicidial_log <  ActiveRecord::Base 
+  set_table_name "vicidial_log"
+end
+class Vicidial_call_log <  ActiveRecord::Base 
+  set_table_name "call_log"
 end
 
-phones = Phones.find(:first)
-#puts "the first campaign is #{campaign_first}"
 
+three_months_ago = 1.months.ago.strftime("%Y-%m-%d %H:%M:%S")
+
+puts "Deleting vicidial_agent_log three months old record...\n"
+Vicidial_agent_log.delete_all "event_time <= '#{three_months_ago}'"
+
+puts "Deleting vicidial_user_log three months old record...\n"
+Vicidial_user_log.delete_all "event_date <= '#{three_months_ago}'"
+
+puts "Deleting vicidial_admin_log three months old record...\n"
+Vicidial_admin_log.delete_all "event_date <= '#{three_months_ago}'"
+
+puts "Deleting vicidial_log three months old record...\n"
+Vicidial_log.delete_all "call_date <= '#{three_months_ago}'"
+
+puts "Deleting call_log three months old record...\n"
+Vicidial_call_log.delete_all "start_time <= '#{three_months_ago}'"
+
+puts
+puts "Done...\n"
 
